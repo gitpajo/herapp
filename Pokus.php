42d7eed667cf11b0bc928054f60c66bbdeede8f8
Pavel Jorda <pavel.jorda@synot.cz>
2015-06-02 17:24:24 +0200

    Add tests

---
+++ Pokus.php
@@ -0,0 +1,24 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of Pokus
+ *
+ * @author pavel.jorda
+ */
+class Pokus {
+
+  // property declaration
+  public $var = 'dlouhytestovacitext';
+
+  // method declaration
+  public function displayVar() {
+    return $this->var;
+  }
+
+}
