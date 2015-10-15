42fe88ae6beeaecbf73ad92efa6ef5aba5aefde0
Pavel Jorda <pavel.jorda@synot.cz>
2015-10-15 16:37:21 +0200

    Add Logger

--- front.php
+++ front.php
@@ -1,5 +1,11 @@
 <?php
 
+require __DIR__."/vendor/autoload.php";
+
+$log = new Monolog\Logger('Log');
+$log->pushHandler(new Monolog\Handler\StreamHandler('herapp.log', Monolog\Logger::INFO));
+$log->addInfo('Start App');
+
 $path = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 
 if (preg_match('/messages\/\d+/', $path)) {
